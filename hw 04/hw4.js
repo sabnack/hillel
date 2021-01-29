3ba2013f666dda29d950097ff2af79791fc5e553
sabnock <sgorozhankin@gmail.com>
2021-01-29 19:10:04 +0200

    hw 4

---
+++ hw 04/hw4.js
@@ -0,0 +1,70 @@
+// В двумерном массиве A размером n(рядов) на m(столбцов). Заполнить случайными числами.
+// Найти ряд, где сумма элементов наименьшая
+// Найти ряд, где сумма элементов найбольшая
+// Поменять ряды местами (1 и 2 пункт)
+var n = 5,
+    m = 6,
+    array = [],
+    minSum = 0,
+    maxSum = 0,
+    minSumIndex,
+    maxSumIndex;
+
+for (var i = 0; i < n; i++) {
+    array[i] = [];
+    for (var j = 0; j < m; j++) {
+        array[i][j] = Math.round(Math.random() * 100);
+    }
+    var sum = array[i].reduce((sum, current) => sum + current, 0);
+    if (minSum > sum || i === 0) {
+        minSum = sum;
+        minSumIndex = i;
+    }
+
+    if (maxSum < sum) {
+        maxSum = sum;
+        maxSumIndex = i;
+    }
+}
+
+console.log(array[minSumIndex], ' minSum = ' + minSum + ' Min index = ' + minSumIndex);
+console.log(array[maxSumIndex], ' maxSum = ' + maxSum + ' Max index = ' + maxSumIndex);
+
+var swap = array[maxSumIndex];
+array[maxSumIndex] = array[minSumIndex];
+array[minSumIndex] = swap;
+
+// Создать двумерный массив, заполненный случайными числами в любом диапазоне.
+// Найти колонку, где сумма элементов наименьшая.
+// Найти колонку, где сумма элементов найбольшая.
+console.log(array);
+minSum = maxSum = 0;
+for (var j = 0; j < m; j++) {
+    for (var i = 0, sum = 0; i < n; i++) {
+        sum += array[i][j];
+    }
+
+    if (minSum > sum || j === 0) {
+        minSum = sum;
+        minSumIndex = j;
+    }
+
+    if (maxSum < sum) {
+        maxSum = sum;
+        maxSumIndex = j;
+    }
+}
+
+console.log('MinSum Column = ' + minSum + ' index = ', minSumIndex);
+console.log('MaxSum Column = ' + maxSum + ' index = ', maxSumIndex);
+
+// В одномерном массиве произвести такую замену:
+// 1 элемент поменять с 2
+// 3 элемент поменять с 4
+// 5 элемент поменять с 6 и тд
+// Если массив непарный - последний элемент не трогать.
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (var i = 0; i < arr.length - 1; i += 2) {
+    arr.splice(i + 1, 0, arr.splice(i, 1)[0]);
+}
+console.log(arr);
---
+++ test/test.js
@@ -0,0 +1,28 @@
+// 1
+var array = [1, 2, 3, 4, 5];
+var stringArray = '<pre>' + array.join('_') + '</pre>';
+document.writeln(stringArray);
+// 2
+var array = [6, 7, 2, 3, 5, 7];
+var stringArray = '<pre><ul>';
+for (var i = 0; i < array.length; i++) {
+    stringArray += '<li>' + array[i] + '</li>';
+}
+stringArray += '</ul></pre>';
+document.writeln(stringArray);
+// 3
+var array = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(array);
+var delIndex = array.length / 2,
+    delCount = 2;
+array.length % 2 === 0 ? delIndex -= 1 : delCount -= 1;
+array.splice(delIndex, delCount);
+console.log(array);
+// 4
+var array = [1, 2, 3];
+var input;
+do {
+    input = prompt('Enter value: ', '');
+    array.push(input);
+} while (input !== '&')
+console.log(array);
