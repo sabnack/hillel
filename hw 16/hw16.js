06ddfecf8c6a726258d6d744788cdb97d0f02abc
sabnock <sgorozhankin@gmail.com>
2021-03-13 20:00:09 +0200

    hw 16

---
+++ hw 16/hw16.js
@@ -0,0 +1,10 @@
+const elements = document.querySelectorAll('input');
+for(let element of elements){
+    setInterval(timer.bind(element), 2000);
+}
+
+function timer() {    
+    let textArea = document.getElementById('text-area');
+    let res = textArea.value;
+    textArea.value = res + ',' + this.value;
+}
