3356bf5d381392539eee9275711fd1ed9d98ffd0
sabnock <sgorozhankin@gmail.com>
2021-03-23 21:44:38 +0200

    fix

--- hw 16/hw16.js
+++ hw 16/hw16.js
@@ -3,9 +3,10 @@ const textArea = document.getElementById('text-area');
 setInterval(timer.bind(elements), 2000);
 
 function timer() {
+    textArea.value = '';
     for (let element of this) {
         if (element.value) {
-            textArea.value? textArea.value += `,${element.value}`: textArea.value += element.value;
+            textArea.value ? textArea.value += `,${element.value}` : textArea.value += element.value;
         }
     }
 }
